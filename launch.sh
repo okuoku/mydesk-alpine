2f6a632ee1ab622c90e4b039f362d96954180ac9
okuoku <mjt@cltn.org>
2025-12-07 11:08:53 +0900

    Add `XDG_MENU_PREFIX` env

--- launch.sh
+++ launch.sh
@@ -4,6 +4,7 @@ docker run -it --rm \
     -u 0:0 \
     -e XDG_RUNTIME_DIR=/runtime \
     -e XDG_CONFIG_DIRS=/etc:/etc/xdg:/usr/share \
+    -e XDG_MENU_PREFIX=lxqt- \
     -e QT_PLATFORM_PLUGIN=lxqt \
     -e QT_QPA_PLATFORMTHEME=lxqt \
     -e WAYLAND_DISPLAY=wayland-HOST \
