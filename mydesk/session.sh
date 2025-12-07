75879b2a32a18adaf69d1059cfa0a6625db1df63
okuoku <mjt@cltn.org>
2025-12-07 23:25:13 +0900

    mydesk: Fix Fcitx5 start timing

--- mydesk/session.sh
+++ mydesk/session.sh
@@ -3,8 +3,5 @@ export FCITX_ADDON_DIRS=/opt/skk/lib/fcitx5:/usr/lib/fcitx5
 export XDG_DATA_DIRS=/opt/skk/share:/usr/local/share:/usr/share
 eval `dbus-launch --auto-syntax`
 
-# DBus clients (Move to labwc autolaunch)
-fcitx5 -d
-
 # Launch compositor
 exec labwc
