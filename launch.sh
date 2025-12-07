ed2caf9d612a7d1a1ce919bfb00bc09a6073acaf
okuoku <mjt@cltn.org>
2025-12-07 23:25:00 +0900

    launch: More shared directories

--- launch.sh
+++ launch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir -p `dirname $0`/runtime
+mkdir -p `dirname $0`/files
+
 docker run -it --rm \
     -u 0:0 \
     -e XDG_RUNTIME_DIR=/runtime \
@@ -11,6 +14,8 @@ docker run -it --rm \
     -e PULSE_COOKIE=/runtime/PulseCookie \
     -e PULSE_LATENCY_MS=50 \
     -e LANG=ja_JP.utf8 \
+    -v `dirname $0`/runtime:/runtime \
+    -v `dirname $0`/files:/files \
     -v `dirname $0`/config_dir:/home/.config \
     -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/runtime/wayland-HOST \
     -v /dev/dri:/dev/dri:rw \
