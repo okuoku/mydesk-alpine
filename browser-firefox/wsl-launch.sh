ad9368f26713ed8a8d37e1d312d941fd29b9e48f
okuoku <mjt@cltn.org>
2025-12-11 21:22:27 +0900

    firefox: Update to support WSLg

---
+++ browser-firefox/wsl-launch.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir -p `dirname $0`/../home_dir_firefox
+
+docker run -it --rm \
+    -u 0:0 \
+    -e XDG_RUNTIME_DIR=/runtime \
+    -e XDG_CONFIG_DIRS=/etc:/etc/xdg:/usr/share \
+    -e DBUS_SESSION_BUS_ADDRESS=unix:path=/runtime/dbus \
+    -e WAYLAND_DISPLAY=wayland-0 \
+    -e HOME=/home \
+    -e PULSE_SERVER=/runtime/PulseAudio \
+    -e PULSE_COOKIE=/runtime/PulseCookie \
+    -e PULSE_LATENCY_MS=50 \
+    -e LANG=C \
+    -v `dirname $0`/../runtime:/runtime \
+    -v `dirname $0`/../files:/files \
+    -v `dirname $0`/../home_dir_firefox:/home \
+    -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/runtime/wayland-0 \
+    -v $XDG_RUNTIME_DIR/pulse/native:/runtime/PulseAudio \
+    firefox firefox
+    
+    #-v /dev/dri:/dev/dri:rw \
+    #-v $HOME/.config/pulse/cookie:/runtime/PulseCookie \
